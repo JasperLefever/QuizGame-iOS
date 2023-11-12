7b8b78d3bb87f6945b959e5404d8d5aae980aa47
JasperLefever <jasper.lefever@telenet.be>
2023-11-12 16:30:46 +0100

    Initial Commit

---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Jasper Lefever on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
